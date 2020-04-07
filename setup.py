3cc1d0d2c1f7f603663a4ab61468a5b2ead88901
Aleix <333aleix333@gmail.com>
2020-04-07 16:25:45 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='nteu_gateway',
+    version='0.1',
+    description='NTEU gateway',
+    url='https://github.com/Pangeamt/nteu_gateway',
+    author='PangeaMT',
+    author_email='a.cerda@pangeanic.es',
+    license='MIT',
+    packages=['nteu_gateway'],
+    zip_safe=False
+)
